37e73affb9f81f96a1e57769681786af9c93d021
Jabir Omar <jabiromar400@gmail.com>
2022-09-22 11:22:54 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+
+
+import React, { useReducer } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -11,8 +13,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name = {user.name} city = {user.city} color = {user.color} />
+      <About bio = {user.bio} links = {user.links}/>
     </div>
   );
 }
